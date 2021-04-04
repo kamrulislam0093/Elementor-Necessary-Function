1ea948f3d7b249b7c1216f741c0b33fcb42676ab
Kamrul Islam <42846042+kamrulislam0093@users.noreply.github.com>
2021-04-04 12:50:36 +0600

    Update display-frontend-content.php

--- display-frontend-content.php
+++ display-frontend-content.php
@@ -44,3 +44,30 @@ function rselements_shortcode_insert_elementor($post_id){
 }
 // rselements_shortcode_insert_elementor(52)
 
+add_action( 'wp_footer', function(){
+
+
+    if(!class_exists('Elementor\Plugin')){
+        return '';
+    }
+    
+    $response = Plugin::instance()->frontend->get_builder_content_for_display(55);
+    var_dump( $response );
+
+});
+
+
+
+add_action( 'manage_elementor_library_posts_columns', function( $defaults ){
+    $defaults['shortcode'] = __( 'Shortcode', 'elementor-pro' );
+
+    return $defaults;
+});
+
+add_action( 'manage_elementor_library_posts_custom_column', function($column_name, $post_id){
+    if ( 'shortcode' === $column_name ) {
+      // %s = shortcode, %d = post_id
+      $shortcode = esc_attr( sprintf( '[%s id="%d"]', 'elementor-template', $post_id ) );
+      printf( '<input class="elementor-shortcode-input" type="text" readonly onfocus="this.select()" value="%s" />', $shortcode );
+    }  
+}, 10, 2 );
